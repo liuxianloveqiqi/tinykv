aa7d895c61010d78a958f3014048dce8b4e6f71f
liuxian <liu_xian.123@qq.com>
2024-08-09 13:21:42 +0800

    feat: project4 finished

--- kv/server/server.go
+++ kv/server/server.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"context"
+	"github.com/pingcap-incubator/tinykv/kv/transaction/mvcc"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 
 	"github.com/pingcap-incubator/tinykv/kv/coprocessor"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
@@ -50,37 +52,516 @@ func (server *Server) Snapshot(stream tinykvpb.TinyKv_SnapshotServer) error {
 // Transactional API.
 func (server *Server) KvGet(_ context.Context, req *kvrpcpb.GetRequest) (*kvrpcpb.GetResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	getResponse := &kvrpcpb.GetResponse{}
+	// 从存储中获取一个 Reader，用于读取数据
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			getResponse.RegionError = regionErr.RequestErr
+		}
+		return getResponse, err
+	}
+	defer reader.Close()
+
+	// 创建一个新的 MVCC 事务，使用获取的 Reader 和请求中的版本号
+	txn := mvcc.NewMvccTxn(reader, req.Version)
+
+	// 获取键上的锁信息
+	lock, err := txn.GetLock(req.Key)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			getResponse.RegionError = regionErr.RequestErr
+		}
+		return getResponse, err
+	}
+
+	// 如果存在锁，并且 startTs 小于当前的 startTs，说明存在你之前存在尚未 commit 的请求，读了就会发生脏读
+	if lock != nil && lock.Ts <= req.Version {
+		getResponse.Error = &kvrpcpb.KeyError{
+			Locked: &kvrpcpb.LockInfo{
+				PrimaryLock: lock.Primary,
+				LockVersion: lock.Ts,
+				Key:         req.Key,
+				LockTtl:     lock.Ttl,
+			}}
+		// 返回响应
+		return getResponse, nil
+	}
+
+	// 获取键对应的值
+	value, err := txn.GetValue(req.Key)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			getResponse.RegionError = regionErr.RequestErr
+			return getResponse, nil
+		}
+		return nil, err
+	}
+
+	// 如果值为 nil，则设置响应中的 NotFound 字段为 true
+	if value == nil {
+		getResponse.NotFound = true
+	}
+	getResponse.Value = value
+	return getResponse, nil
 }
 
 func (server *Server) KvPrewrite(_ context.Context, req *kvrpcpb.PrewriteRequest) (*kvrpcpb.PrewriteResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	resp := &kvrpcpb.PrewriteResponse{}
+	if len(req.Mutations) == 0 {
+		return resp, nil
+	}
+
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer reader.Close()
+
+	// 创建一个新的 MVCC 事务，使用获取的 Reader 和请求中的 StartVersion
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+
+	var keyErrors []*kvrpcpb.KeyError
+
+	for _, m := range req.Mutations {
+		// 获取该key的最近一次写入
+		write, commitTs, err := txn.MostRecentWrite(m.Key)
+		if err != nil {
+			// 如果获取写入时发生错误，并且错误是 RegionError 类型，则设置响应中的 RegionError 字段
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+
+		// 如果存在写入并且请求并且commitTs >=的 startTs，
+		// 说明在该事务的更新过程中其他事务提交过对该key的修改，返回WriteConflict错误
+		if write != nil && commitTs >= req.StartVersion {
+			keyErrors = append(keyErrors, &kvrpcpb.KeyError{
+				Conflict: &kvrpcpb.WriteConflict{
+					StartTs:    req.StartVersion,
+					ConflictTs: commitTs,
+					Key:        m.Key,
+					Primary:    req.PrimaryLock,
+				}})
+			continue
+		}
+
+		// 获取键上的锁信息
+		lock, err := txn.GetLock(m.Key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+
+		// 如果存在lock说明当前 key 被其他事务使用中
+		if lock != nil {
+			keyErrors = append(keyErrors, &kvrpcpb.KeyError{
+				Locked: &kvrpcpb.LockInfo{
+					PrimaryLock: lock.Primary,
+					LockVersion: lock.Ts,
+					Key:         m.Key,
+					LockTtl:     lock.Ttl,
+				}})
+			continue
+		}
+
+		// 根据 Mutation 的操作类型设置写入类型
+		var kind mvcc.WriteKind
+		switch m.Op {
+		case kvrpcpb.Op_Put:
+			kind = mvcc.WriteKindPut
+			txn.PutValue(m.Key, m.Value)
+		case kvrpcpb.Op_Del:
+			kind = mvcc.WriteKindDelete
+			txn.DeleteValue(m.Key)
+		default:
+			return nil, nil
+		}
+
+		// 在事务中添加锁
+		txn.PutLock(m.Key, &mvcc.Lock{
+			Primary: req.PrimaryLock,
+			Ts:      req.StartVersion,
+			Ttl:     req.LockTtl,
+			Kind:    kind})
+	}
+
+	// 如果存在键错误，设置响应中的 Errors 字段并返回响应
+	if len(keyErrors) > 0 {
+		resp.Errors = keyErrors
+		return resp, nil
+	}
+
+	// 将事务中的写入操作写入存储
+	err = server.storage.Write(req.Context, txn.Writes())
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+
+	// 返回响应
+	return resp, nil
 }
 
 func (server *Server) KvCommit(_ context.Context, req *kvrpcpb.CommitRequest) (*kvrpcpb.CommitResponse, error) {
 	// Your Code Here (4B).
-	return nil, nil
+	resp := &kvrpcpb.CommitResponse{}
+	if len(req.Keys) == 0 {
+		return resp, nil
+	}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer reader.Close()
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+	/*
+		TinyKV 可以同时处理多个请求，因此存在本地竞争条件的可能性。
+		例如，TinyKV 可能会同时收到来自不同客户端的两个请求，其中一个提交一个密钥，
+		另一个回滚相同的密钥。为避免竞争条件，您可以锁存数据库中的任何键。
+		这个锁存器的工作方式很像每个键的互斥锁。
+		一个锁覆盖所有 CF。Latches.go定义了一个Latches为此提供 API的对象
+	*/
+	server.Latches.WaitForLatches(req.Keys)
+	defer server.Latches.ReleaseLatches(req.Keys)
+	for _, key := range req.Keys {
+		lock, err := txn.GetLock(key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		if lock == nil {
+			continue
+		}
+		// 如果锁的时间戳不等于请求的 StartVersion，说明锁不属于当前事务
+		if lock.Ts != req.StartVersion {
+			resp.Error = &kvrpcpb.KeyError{Retryable: "true"}
+			write, _, err := txn.CurrentWrite(key)
+			if err != nil {
+				if regionErr, ok := err.(*raft_storage.RegionError); ok {
+					resp.RegionError = regionErr.RequestErr
+					return resp, nil
+				}
+				return nil, err
+			}
+			if write != nil {
+				if write.Kind == mvcc.WriteKindRollback {
+					resp.Error = &kvrpcpb.KeyError{Retryable: "false"}
+				}
+			}
+			return resp, nil
+		}
+		txn.PutWrite(key, req.CommitVersion, &mvcc.Write{
+			StartTS: req.StartVersion,
+			Kind:    lock.Kind,
+		})
+		// 删除键上的锁
+		txn.DeleteLock(key)
+	}
+	err = server.storage.Write(req.Context, txn.Writes())
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (server *Server) KvScan(_ context.Context, req *kvrpcpb.ScanRequest) (*kvrpcpb.ScanResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := &kvrpcpb.ScanResponse{}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer reader.Close()
+	txn := mvcc.NewMvccTxn(reader, req.Version)
+
+	scanner := mvcc.NewScanner(req.StartKey, txn)
+	defer scanner.Close()
+
+	var pairs []*kvrpcpb.KvPair
+	limit := int(req.Limit)
+	for i := 0; i < limit; {
+		key, value, err := scanner.Next()
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		if key == nil {
+			break
+		}
+
+		lock, err := txn.GetLock(key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		if lock != nil && req.Version >= lock.Ts {
+			pairs = append(pairs, &kvrpcpb.KvPair{
+				Error: &kvrpcpb.KeyError{
+					Locked: &kvrpcpb.LockInfo{
+						PrimaryLock: lock.Primary,
+						LockVersion: lock.Ts,
+						Key:         key,
+						LockTtl:     lock.Ttl,
+					}},
+				Key: key,
+			})
+			i++
+			continue
+		}
+		if value != nil {
+			pairs = append(pairs, &kvrpcpb.KvPair{Key: key, Value: value})
+			i++
+		}
+	}
+	resp.Pairs = pairs
+	return resp, nil
 }
 
 func (server *Server) KvCheckTxnStatus(_ context.Context, req *kvrpcpb.CheckTxnStatusRequest) (*kvrpcpb.CheckTxnStatusResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := &kvrpcpb.CheckTxnStatusResponse{}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer reader.Close()
+	txn := mvcc.NewMvccTxn(reader, req.LockTs)
+
+	lock, err := txn.GetLock(req.PrimaryKey)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	if lock == nil {
+		write, ts, err := txn.CurrentWrite(req.PrimaryKey)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		if write != nil {
+			if write.Kind != mvcc.WriteKindRollback {
+				resp.CommitVersion = ts
+			}
+			return resp, nil
+		}
+		txn.PutWrite(req.PrimaryKey, req.LockTs, &mvcc.Write{
+			StartTS: req.LockTs,
+			Kind:    mvcc.WriteKindRollback,
+		})
+		err = server.storage.Write(req.Context, txn.Writes())
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		resp.Action = kvrpcpb.Action_LockNotExistRollback
+		return resp, nil
+	}
+	resp.LockTtl = lock.Ttl
+	if mvcc.PhysicalTime(lock.Ts)+lock.Ttl <= mvcc.PhysicalTime(req.CurrentTs) {
+		txn.DeleteLock(req.PrimaryKey)
+		txn.DeleteValue(req.PrimaryKey)
+		txn.PutWrite(req.PrimaryKey, req.LockTs, &mvcc.Write{
+			StartTS: req.LockTs,
+			Kind:    mvcc.WriteKindRollback,
+		})
+		err = server.storage.Write(req.Context, txn.Writes())
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		resp.LockTtl = 0
+		resp.Action = kvrpcpb.Action_TTLExpireRollback
+	}
+	return resp, nil
 }
 
 func (server *Server) KvBatchRollback(_ context.Context, req *kvrpcpb.BatchRollbackRequest) (*kvrpcpb.BatchRollbackResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := &kvrpcpb.BatchRollbackResponse{}
+	if len(req.Keys) == 0 {
+		return resp, nil
+	}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer reader.Close()
+	txn := mvcc.NewMvccTxn(reader, req.StartVersion)
+	server.Latches.WaitForLatches(req.Keys)
+	defer server.Latches.ReleaseLatches(req.Keys)
+
+	for _, key := range req.Keys {
+		write, _, err := txn.CurrentWrite(key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		if write != nil {
+			if write.Kind == mvcc.WriteKindRollback {
+				continue
+			} else {
+				txn.DeleteValue(key)
+				txn.PutWrite(key, req.StartVersion, &mvcc.Write{
+					StartTS: req.StartVersion,
+					Kind:    mvcc.WriteKindRollback,
+				})
+				resp.Error = &kvrpcpb.KeyError{Abort: "true"}
+				return resp, nil
+			}
+		}
+		lock, err := txn.GetLock(key)
+		if err != nil {
+			if regionErr, ok := err.(*raft_storage.RegionError); ok {
+				resp.RegionError = regionErr.RequestErr
+				return resp, nil
+			}
+			return nil, err
+		}
+		txn.PutWrite(key, req.StartVersion, &mvcc.Write{
+			StartTS: req.StartVersion,
+			Kind:    mvcc.WriteKindRollback,
+		})
+		if lock == nil || lock.Ts != req.StartVersion {
+			continue
+		}
+		txn.DeleteLock(key)
+		txn.DeleteValue(key)
+	}
+	err = server.storage.Write(req.Context, txn.Writes())
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	return resp, nil
 }
 
 func (server *Server) KvResolveLock(_ context.Context, req *kvrpcpb.ResolveLockRequest) (*kvrpcpb.ResolveLockResponse, error) {
 	// Your Code Here (4C).
-	return nil, nil
+	resp := &kvrpcpb.ResolveLockResponse{}
+	if req.StartVersion == 0 {
+		return resp, nil
+	}
+	reader, err := server.storage.Reader(req.Context)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer reader.Close()
+	iter := reader.IterCF(engine_util.CfLock)
+	if err != nil {
+		if regionErr, ok := err.(*raft_storage.RegionError); ok {
+			resp.RegionError = regionErr.RequestErr
+			return resp, nil
+		}
+		return nil, err
+	}
+	defer iter.Close()
+
+	var keys [][]byte
+	for ; iter.Valid(); iter.Next() {
+		item := iter.Item()
+		value, err := item.ValueCopy(nil)
+		if err != nil {
+			return resp, err
+		}
+		lock, err := mvcc.ParseLock(value)
+		if err != nil {
+			return resp, err
+		}
+		if lock.Ts == req.StartVersion {
+			key := item.KeyCopy(nil)
+			keys = append(keys, key)
+		}
+	}
+	if len(keys) == 0 {
+		return resp, nil
+	}
+	// If commit_version is 0, TinyKV will rollback all locks.
+	// else it will commit those locks with the given commit timestamp.
+	if req.CommitVersion == 0 {
+		resp1, err := server.KvBatchRollback(nil, &kvrpcpb.BatchRollbackRequest{
+			Context:      req.Context,
+			StartVersion: req.StartVersion,
+			Keys:         keys,
+		})
+		resp.Error = resp1.Error
+		resp.RegionError = resp1.RegionError
+		return resp, err
+	} else {
+		resp1, err := server.KvCommit(nil, &kvrpcpb.CommitRequest{
+			Context:       req.Context,
+			StartVersion:  req.StartVersion,
+			Keys:          keys,
+			CommitVersion: req.CommitVersion,
+		})
+		resp.Error = resp1.Error
+		resp.RegionError = resp1.RegionError
+		return resp, err
+	}
 }
 
 // SQL push down commands.
--- kv/transaction/mvcc/transaction.go
+++ kv/transaction/mvcc/transaction.go
@@ -1,7 +1,9 @@
 package mvcc
 
 import (
+	"bytes"
 	"encoding/binary"
+	"github.com/pingcap-incubator/tinykv/kv/util/engine_util"
 
 	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/kv/util/codec"
@@ -41,46 +43,156 @@ func (txn *MvccTxn) Writes() []storage.Modify {
 // PutWrite records a write at key and ts.
 func (txn *MvccTxn) PutWrite(key []byte, ts uint64, write *Write) {
 	// Your Code Here (4A).
+	// ts是commitTs,write里面包含 startTs
+	modify := storage.Modify{
+		Data: storage.Put{
+			Key:   EncodeKey(key, ts),
+			Cf:    engine_util.CfWrite,
+			Value: write.ToBytes(),
+		},
+	}
+	txn.writes = append(txn.writes, modify)
 }
 
 // GetLock returns a lock if key is locked. It will return (nil, nil) if there is no lock on key, and (nil, err)
 // if an error occurs during lookup.
 func (txn *MvccTxn) GetLock(key []byte) (*Lock, error) {
 	// Your Code Here (4A).
-	return nil, nil
+	// 从存储中获取指定列族（CfLock）和键的值。
+	lock, err := txn.Reader.GetCF(engine_util.CfLock, key)
+	// 如果获取过程中发生错误，返回错误。
+	if err != nil {
+		return nil, err
+	}
+	// 如果没有找到锁，返回 (nil, nil)。
+	if lock == nil {
+		return nil, nil
+	}
+	// 解析锁对象，如果解析过程中发生错误，返回错误。
+	parseLock, err := ParseLock(lock)
+	if err != nil {
+		return nil, err
+	}
+	return parseLock, nil
 }
 
 // PutLock adds a key/lock to this transaction.
 func (txn *MvccTxn) PutLock(key []byte, lock *Lock) {
 	// Your Code Here (4A).
+	modify := storage.Modify{
+		Data: storage.Put{
+			Key:   key,
+			Cf:    engine_util.CfLock,
+			Value: lock.ToBytes(),
+		},
+	}
+	txn.writes = append(txn.writes, modify)
 }
 
 // DeleteLock adds a delete lock to this transaction.
 func (txn *MvccTxn) DeleteLock(key []byte) {
 	// Your Code Here (4A).
+	modify := storage.Modify{
+		Data: storage.Delete{
+			Key: key,
+			Cf:  engine_util.CfLock,
+		},
+	}
+	txn.writes = append(txn.writes, modify)
 }
 
 // GetValue finds the value for key, valid at the start timestamp of this transaction.
 // I.e., the most recent value committed before the start of this transaction.
 func (txn *MvccTxn) GetValue(key []byte) ([]byte, error) {
 	// Your Code Here (4A).
-	return nil, nil
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	defer iter.Close()
+	// 将迭代器定位到第一个大于等于编码后的键的位置
+	// 时间戳被编码成大端序，并取反，以确保较新的时间戳排在前面
+	// 利用迭代器遍历 Write，找到 Write的commitTs <= 当前ts 最新 Write
+	iter.Seek(EncodeKey(key, txn.StartTS))
+	if !iter.Valid() {
+		return nil, nil
+	}
+	item := iter.Item()
+	gotKey := item.KeyCopy(nil)
+	userKey := DecodeUserKey(gotKey)
+	// 如果解码后Key与传入的键不相等，返回 nil
+	if !bytes.Equal(key, userKey) {
+		return nil, nil
+	}
+	value, err := item.ValueCopy(nil)
+	if err != nil {
+		return nil, err
+	}
+	write, err := ParseWrite(value)
+	if err != nil {
+		return nil, err
+	}
+	// 如果写入对象的类型是Delete，返回 nil。
+	if write.Kind == WriteKindDelete {
+		return nil, nil
+	}
+	return txn.Reader.GetCF(engine_util.CfDefault, EncodeKey(key, write.StartTS))
 }
 
 // PutValue adds a key/value write to this transaction.
 func (txn *MvccTxn) PutValue(key []byte, value []byte) {
 	// Your Code Here (4A).
+	modify := storage.Modify{
+		Data: storage.Put{
+			Key:   EncodeKey(key, txn.StartTS),
+			Cf:    engine_util.CfDefault,
+			Value: value,
+		},
+	}
+	txn.writes = append(txn.writes, modify)
 }
 
 // DeleteValue removes a key/value pair in this transaction.
 func (txn *MvccTxn) DeleteValue(key []byte) {
 	// Your Code Here (4A).
+	modify := storage.Modify{
+		Data: storage.Delete{
+			Key: EncodeKey(key, txn.StartTS),
+			Cf:  engine_util.CfDefault,
+		},
+	}
+	txn.writes = append(txn.writes, modify)
 }
 
 // CurrentWrite searches for a write with this transaction's start timestamp. It returns a Write from the DB and that
 // write's commit timestamp, or an error.
 func (txn *MvccTxn) CurrentWrite(key []byte) (*Write, uint64, error) {
 	// Your Code Here (4A).
+	// 创建一个迭代器，用于遍历写列族（CfWrite）。
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	defer iter.Close()
+	// 将迭代器定位到第一个大于等于编码后的键的位置。
+	// TsMax 表示最大的时间戳。
+	for iter.Seek(EncodeKey(key, TsMax)); iter.Valid(); iter.Next() {
+		item := iter.Item()
+		gotKey := item.KeyCopy(nil)
+		userKey := DecodeUserKey(gotKey)
+		// 如果解码后的用户键与传入的键不相等，返回 nil。
+		if !bytes.Equal(key, userKey) {
+			return nil, 0, nil
+		}
+		value, err := item.ValueCopy(nil)
+		if err != nil {
+			return nil, 0, err
+		}
+		// 解析写入对象。
+		write, err := ParseWrite(value)
+		if err != nil {
+			return nil, 0, err
+		}
+		// 如果写入对象的 startTS 与事务的 startTS 相等，返回写入对象和解码后的时间戳。
+		if write.StartTS == txn.StartTS {
+			return write, decodeTimestamp(gotKey), nil
+		}
+	}
+	// 如果没有找到匹配的写入对象，返回 nil。
 	return nil, 0, nil
 }
 
@@ -88,7 +200,27 @@ func (txn *MvccTxn) CurrentWrite(key []byte) (*Write, uint64, error) {
 // write's commit timestamp, or an error.
 func (txn *MvccTxn) MostRecentWrite(key []byte) (*Write, uint64, error) {
 	// Your Code Here (4A).
-	return nil, 0, nil
+	iter := txn.Reader.IterCF(engine_util.CfWrite)
+	defer iter.Close()
+	iter.Seek(EncodeKey(key, TsMax))
+	if !iter.Valid() {
+		return nil, 0, nil
+	}
+	item := iter.Item()
+	gotKey := item.KeyCopy(nil)
+	userKey := DecodeUserKey(gotKey)
+	if !bytes.Equal(key, userKey) {
+		return nil, 0, nil
+	}
+	value, err := item.ValueCopy(nil)
+	if err != nil {
+		return nil, 0, err
+	}
+	write, err := ParseWrite(value)
+	if err != nil {
+		return nil, 0, err
+	}
+	return write, decodeTimestamp(gotKey), nil
 }
 
 // EncodeKey encodes a user key and appends an encoded timestamp to a key. Keys and timestamps are encoded so that
